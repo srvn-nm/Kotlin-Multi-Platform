ab9e016c7ef091afcc4cae727486c394f92573a0
s_nami <s_nami@tamin.org>
2025-11-16 08:43:43 +0330

    the shared Main Activity of the app

--- composeApp/src/androidMain/kotlin/com/example/kmp/ui/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/example/kmp/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.kmp
+package com.example.kmp.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.kmp.App
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,7 +14,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            App()
+            LoginScreen()
         }
     }
 }
