c5e524873a08c08723371c00082c9fcf70b80b12
s_nami <s_nami@tamin.org>
2025-11-16 08:41:54 +0330

    the shared dependencies of the app

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,3 +1,4 @@
+import org.gradle.kotlin.dsl.kotlin
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
@@ -30,6 +31,44 @@ kotlin {
     sourceSets {
         commonMain.dependencies {
             // put your Multiplatform dependencies here
+            // Authentication JWT
+            implementation(libs.ktor.server.auth.jwt.jvm)
+            implementation(libs.ktor.server.auth.jvm)
+
+            // Logging
+            implementation(libs.logback)
+
+            // Database: Exposed + HikariCP + Postgres driver
+            implementation(libs.hikaricp)
+            implementation(libs.postgresql)
+
+            // Password hashing (bcrypt) - favre library
+            implementation(libs.bcrypt)
+
+            implementation(libs.ktor.server.error.handler)
+
+            implementation(libs.ktor.server.auth)
+            implementation(libs.ktor.server.auth.jwt)
+
+            implementation(libs.ktor.server.core.jvm.v300)
+            implementation(libs.ktor.server.netty.jvm.v300)
+
+            // JSON
+            implementation(libs.ktor.serialization.kotlinx.json.jvm.v300)
+            implementation(libs.ktor.server.content.negotiation.jvm.v300)
+
+            // Exposed ORM
+            implementation(libs.exposed.core.v0560)
+            implementation(libs.exposed.dao.v0560)
+            implementation(libs.exposed.jdbc.v0560)
+
+            implementation(libs.h2)
+
+            implementation(libs.ktor.client.core)
+            implementation(libs.ktor.client.content.negotiation)
+            implementation(libs.ktor.serialization.kotlinx.json)
+            implementation(libs.ktor.client.auth)
+            implementation(libs.ktor.client.auth.resources)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
