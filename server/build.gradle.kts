f05c519e00c4b9429c07da31cf83f4ff4e5d4b3a
s_nami <s_nami@tamin.org>
2025-11-16 07:58:23 +0330

    the server's dependencies

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -14,10 +14,44 @@ application {
 }
 
 dependencies {
+
+    // Authentication JWT
+    implementation(libs.ktor.server.auth.jwt.jvm)
+    implementation(libs.ktor.server.auth.jvm)
+
+    // Logging
+    implementation(libs.logback)
+
+    // Database: Exposed + HikariCP + Postgres driver
+    implementation(libs.hikaricp)
+    implementation(libs.postgresql)
+
+    // Password hashing (bcrypt) - favre library
+    implementation(libs.bcrypt)
+
+    implementation(libs.ktor.server.error.handler)
+
+    // Testing (optional)
+    testImplementation(kotlin("test"))
     implementation(projects.shared)
     implementation(libs.logback)
-    implementation(libs.ktor.serverCore)
-    implementation(libs.ktor.serverNetty)
     testImplementation(libs.ktor.serverTestHost)
     testImplementation(libs.kotlin.testJunit)
+
+    implementation(libs.ktor.server.auth)
+    implementation(libs.ktor.server.auth.jwt)
+
+    implementation(libs.ktor.server.core.jvm.v300)
+    implementation(libs.ktor.server.netty.jvm.v300)
+
+    // JSON
+    implementation(libs.ktor.serialization.kotlinx.json.jvm.v300)
+    implementation(libs.ktor.server.content.negotiation.jvm.v300)
+
+    // Exposed ORM
+    implementation(libs.exposed.core.v0560)
+    implementation(libs.exposed.dao.v0560)
+    implementation(libs.exposed.jdbc.v0560)
+
+    implementation(libs.h2)
 }
